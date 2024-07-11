f2a777a8700e202824ff35d96746535538cc5975
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-07-11 15:15:22 -0700

    Update priority featureImage Update works return Update work return Update routes and nav Update title

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { Route, Routes, Link, NavLink } from 'react-router-dom'
 import Home from './templates/Home'
 import About from './templates/About'
-import Posts from './templates/Posts'
-import Post from './templates/Post'
 import Contact from './templates/Contact'
 import Works from './templates/Works'
+import Work from './templates/Work'
 
 function App() {
-
   return (
     <>
       <header id="masthead" className="site-header">
@@ -16,7 +14,6 @@ function App() {
         <nav className="site-navigation">
           <ul>
             <li><NavLink to='/' end>Home</NavLink></li>
-            <li><NavLink to='/blog'>Blog</NavLink></li>
             <li><NavLink to='/about'>About</NavLink></li>
             <li><NavLink to='/contact'>Contact</NavLink></li>
             <li><NavLink to='/works'>Works</NavLink></li>
@@ -27,8 +24,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/blog' element={<Posts />} />
-          <Route path='/blog/:slug' element={<Post />} />
+          <Route path='/works/:slug' element={<Work />} />
           <Route path='/works' element={<Works />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
--- src/utilities/Utilities.jsx
+++ src/utilities/Utilities.jsx
@@ -1,9 +1,10 @@
 export const restBase = 'https://danimelo.ca/portfolio/wp-json/wp/v2/'
 
-export const featuredImage = ( featuredImageObject ) => {
-  let imgWidth = featuredImageObject.media_details.sizes.full.width;
-  let imgHeight = featuredImageObject.media_details.sizes.full.height;
-  let imgURL = featuredImageObject.source_url;
+export const featuredImage = (featuredImageObject) => {
+  const landscapeProject = featuredImageObject.media_details.sizes.landscape_projects;
+  const imgWidth = landscapeProject ? landscapeProject.width : featuredImageObject.media_details.sizes.full.width;
+  const imgHeight = landscapeProject ? landscapeProject.height : featuredImageObject.media_details.sizes.full.height;
+  const imgURL = landscapeProject ? landscapeProject.source_url : featuredImageObject.source_url;
   let img = `<img src="${imgURL}" 
       width="${imgWidth}"
       height="${imgHeight}"
@@ -11,8 +12,8 @@ export const featuredImage = ( featuredImageObject ) => {
       srcset="${imgURL} ${imgWidth}w,
       ${featuredImageObject.media_details.sizes.large ? featuredImageObject.media_details.sizes.large.source_url + ' 1024w,' : ''}
       ${featuredImageObject.media_details.sizes.medium_large ? featuredImageObject.media_details.sizes.medium_large.source_url + ' 768w,' : ''}
-      ${featuredImageObject.media_details.sizes.landscape_projects ? featuredImageObject.media_details.sizes.landscape_projects.source_url + ' 530w,' : ''}"
+      ${landscapeProject ? landscapeProject.source_url + ' 530w,' : ''}
       ${featuredImageObject.media_details.sizes.medium ? featuredImageObject.media_details.sizes.medium.source_url + ' 300w' : ''}"
-      sizes="(max-width: ${imgWidth}) 100vw, ${imgWidth}px">`;
-  return {__html: img}
+      sizes="(max-width: ${imgWidth}px) 100vw, ${imgWidth}px">`;
+  return {__html: img};
 }
