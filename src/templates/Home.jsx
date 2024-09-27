75b222b59901e51a5b814e2e76c9677d2c1c7efc
Dani Melo <156140180+danielacmelo@users.noreply.github.com>
2024-09-26 18:21:45 -0700

    Removed react-helmet-async

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -3,8 +3,6 @@ import Loading from '../utilities/Loading';
 import { restBase } from '../utilities/Utilities';
 import { Link } from 'react-router-dom';
 import 'animate.css';
-import { Helmet } from 'react-helmet-async';
-
 
 const Home = () => {
     const restPath = restBase + 'pages/8'
@@ -27,16 +25,6 @@ const Home = () => {
     
     return (
         <>
-
-        <Helmet>
-            <title>Dani Melo - Front-End Developer</title>
-            <meta name="description" content="Dani Melo's portfolio showcasing early career projects in front-end development, specializing in WordPress, WooCommerce, Shopify, and React. Recent BCIT graduate." />
-            <meta name="keywords" content="Junior Front-End Developer, WordPress, Shopify, E-commerce, React, BCIT Graduate, Web Development, Dani Melo, Daniela Melo" />
-            <meta property="og:title" content="Dani Melo - Front-End Developer" />
-            <meta property="og:description" content="Explore the portfolio of Dani Melo, a recent BCIT graduate and aspiring front-end developer with a passion for web development and e-commerce solutions." />
-            <meta property="og:url" content="https://danimelo.ca/" />
-            <meta property="og:type" content="website" />
-        </Helmet>
 
         { isLoaded ?
             <article className="animate__animated animate__fadeIn" id={`post-${restData.id}`}>
